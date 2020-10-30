7d197f3e1a0821dafb62f6fc17f5d79437879dc8
aaaaaaaka <68629495+aaaaaaaka@users.noreply.github.com>
2020-10-30 14:19:49 +0900

    10.30

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -1,5 +1,69 @@
 $(function(){
+ 
+    let hako = 0;
+
+    function checkBox(){
+        if (hako > 5) {
+         $("#content").text(hako+"コ入っています。箱はいっぱいです。箱を空っぽにします")
+         hako = 0;
+        }else if (hako <= 0){
+           hako=0;
+           $("#content").text(hako+"コ入っていません");
+
+
+        }else{
+          $("#content").text(hako+"コ入っています。まだ入ります");
+        }
+
+
+    }
+
+
+    $("#button01").on("click",function(){
+        checkBox()
+    })
+
+    $("#button02").on("click",function(){
+        hako = hako + 1;
+        checkBox()
+    })
+
+    $("#button03").on("click",function(){
+        hako = hako - 1;
+        checkBox()
+    })
 
 
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
